8d72c53dd34a2f90a947684a1d052469a3d396a0
Allusive <37462003+AllusiveWheat@users.noreply.github.com>
2022-02-01 09:23:57 -0800

    Remove API Key
    Come on really you left in an api key.

--- pinball-cv/formatting_labelbox_data.py
+++ pinball-cv/formatting_labelbox_data.py
@@ -1,8 +1,7 @@
 import labelbox
 
 # Labelbox API Key
-LBL_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJja3k0MG9pNjkyZjYwMHplcGdlM3o2anpyIiwib3JnYW5pemF0aW9uSWQiOiJja3k0MG9ocXEyZjV6MHplcGVsYjk1N3YyIiwiYXBpS2V5SWQiOiJja3k3bDh5ZG5lZG1sMHpidjN0amphaDdsIiwic2VjcmV0IjoiNjIyMTQ1YTQ5ZWQyNWIxOWQwNTljYzFjYTE4YWUxNTAiLCJpYXQiOjE2NDE3NTI4MzQsImV4cCI6MjI3MjkwNDgzNH0.TuRT1LtGorHfaNSsvXA0_1TbX_ro0EY1805usl07ogo"
-
+LBL_API_KEY = "nice api key"
 # Labelbox Client
 lb_client = labelbox.Client(api_key=LBL_API_KEY)
 
